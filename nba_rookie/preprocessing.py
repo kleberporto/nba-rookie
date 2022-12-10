c6695eb3ae5a6249414a9a3716f2c20a4765f38b
Kleber dos Santos <kleber.santos@poatek.com>
2022-12-10 18:56:10 -0300

    Formatting and fix tests

--- nba_rookie/preprocessing.py
+++ nba_rookie/preprocessing.py
@@ -1,8 +1,23 @@
 from pandas import DataFrame
 
-def treat_position(df: DataFrame, pos_column="position", has_second_pos="has_second_position") -> DataFrame:
+
+def treat_position(
+    df: DataFrame, pos_column="position", has_second_pos="has_second_position"
+) -> DataFrame:
+    """Position field preprocessing step:
+    *Drop rows with NaN
+    *Add "has_second_position"(bool) to indicate if a player plays on two positions
+    *Leaves only the first position on the 'position' row
+
+    Args:
+        df (DataFrame): The pandas DataFrame to be processed
+        pos_column (str, optional): The name of the position column. Defaults to "position".
+        has_second_pos (str, optional): Trues if the player has a second position. Defaults to "has_second_position".
+
+    Returns:
+        DataFrame: The transformed pandas DataFrame
+    """
     df.dropna(inplace=True, subset=[pos_column])
-    df[has_second_pos] = df["pos_column"].str.len() > 1
-    df[pos_column] = df["pos_column"].str[0:1]
+    df[has_second_pos] = df[pos_column].str.len() > 1
+    df[pos_column] = df[pos_column].str[0:1]
     return df
-    
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -3,7 +3,6 @@ from pandas import DataFrame
 from nba_rookie.preprocessing import treat_position
 
 
-
 def test__treat_position():
     test_data = {"position": ["C", "G", "F-C", "G-F", "F"]}
     test_df = DataFrame(test_data)
@@ -12,9 +11,16 @@ def test__treat_position():
 
     for position in return_df.position.to_list():
         assert len(position) < 2
-    
-    assert return_df["has_second_position"].tolist() == [False, False, True, True, False]
+
+    assert return_df["has_second_position"].tolist() == [
+        False,
+        False,
+        True,
+        True,
+        False,
+    ]
     assert return_df.position.tolist() == ["C", "G", "F", "G", "F"]
+
 
 def test__treat_position_with_nan():
     test_data = {"position": ["C", nan]}
@@ -22,5 +28,7 @@ def test__treat_position_with_nan():
 
     return_df = treat_position(test_df)
 
-    print(return_df)    
-    assert False
+    print(return_df)
+
+    assert len(return_df) == 1
+    assert return_df.position.tolist() == ["C"]
